8c35e45af584872d6b4e3fe4d2be502863081b37
Fridtjof Ahlswede <to.7droids@googlemail.com>
2012-06-25 18:08:46 +0200

    Current state at the end of sprint 2. - ListView items are read from resource file - First unit test to validate storage of selected item in list view after relaunch of the app

---
+++ Reminder/src/org/sevendroids/android/reminder/activity/ReminderListActivity.java
@@ -0,0 +1,24 @@
+package org.sevendroids.android.reminder.activity;
+
+import org.sevendroids.android.reminder.R;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+/**
+ * With this activity all items managed by the reminder are displayed.
+ * 
+ * @author 7droids.de (FA)
+ * 
+ */
+public class ReminderListActivity extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+	setListAdapter(new ArrayAdapter<String>(this, R.layout.itemlist,
+		getResources().getStringArray(R.array.reminder_items)));
+    }
+}
